634d2f7c44785685774e3074ad66ab63dd10b9a9
longnguyen <clokapp@gmail.com>
2025-03-02 23:38:23 -0800

    Updated versionCode from 17 to 18, and version name from 2.0.0 to 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.kingfu.clok"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "2.0.0"
+        versionCode = 18
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
